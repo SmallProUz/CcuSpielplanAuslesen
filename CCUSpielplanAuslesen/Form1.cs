48528707c15ced9f5a526b72cbac13d24b057eec
m.louis.ch <m.louis.ch@gmail.com>
2022-07-31 09:28:39 +0200

    Teamliste von 2021

--- CCUSpielplanAuslesen/Form1.cs
+++ CCUSpielplanAuslesen/Form1.cs
@@ -72,7 +72,7 @@ namespace CCUSpielplanAuslesen
 
         private void bt_AlleTeams_Click(object sender, EventArgs e)
         {
-            int[] teamListe= { 2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
+            int[] teamListe= { 1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,23,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
             StartFormContent startFormContent = new StartFormContent();
             startFormContent.SourceFile = pathOfFile;
             startFormContent.TargetPath = pathOnly;
